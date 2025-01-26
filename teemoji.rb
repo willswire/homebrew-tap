77c3fd3052fa3767117603aeedc09dd7fec05c5b
William Walker <w_walker@icloud.com>
2025-01-26 17:48:44 -0600

    chore: downgrade teemoji to v0.0.1

--- teemoji.rb
+++ teemoji.rb
@@ -1,17 +1,15 @@
 class Teemoji < Formula
   desc "Like tee but with emojis 🍵"
   homepage "https://github.com/willswire/teemoji"
-  url "https://github.com/willswire/teemoji/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "0ab624dfd16753fc372ea715a1ccd425ed291d721746df4003189fa91a22c804"
+  url "https://github.com/willswire/teemoji/releases/download/v0.0.1/teemoji.tar.gz"
+  sha256 "a0280b65388acfd25fa85e2130cfa31a7926108205fb7611ea71b9842153c1d4"
   license "MIT"
 
   depends_on macos: :big_sur
 
-  def install
-    system "swift", "build", "--configuration", "release", "--disable-sandbox"
-    
+  def install    
     # Explicitly install only the files you actually need
-    libexec.install ".build/release/teemoji", ".build/release/teemoji_teemoji.bundle"
+    libexec.install "teemoji", "teemoji_teemoji.bundle"
     
     # Create a thin wrapper in bin
     bin.write_exec_script libexec/"teemoji"
